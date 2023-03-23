c7fd0a16a1df5ce601f0b4a31e7727a07be10426
Cornelia Bodea <cornelia.bodea.work@gmail.com>
2023-03-23 18:09:41 +0200

    Add types for each resource

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export type Team = {
+	id: string;
+	team: string;
+};
+
+export type Status = {
+	id: string;
+	status: string;
+};
+
+export type Role = {
+	id: string;
+	role: string;
+};
+
+export type Developer = {
+	name: string;
+	email: string;
+	role: string;
+	status: string;
+	team: string;
+};
